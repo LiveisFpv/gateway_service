18e32155bc4e72a920b4fb07fbc034ef7077d4b1
LongDude <teplov.vladimir.2004@mail.ru>
2025-02-25 22:35:12 +0300

    FIX: requeset и потерянный id

--- internal/domain/country.go
+++ internal/domain/country.go
@@ -1,8 +1,7 @@
 package domain
 
 type Country struct {
-	// * ID потерялся по дороге в Get_CountryById
-	// Country_id      int
+	Country_id      int
 	Country_title   string
 	Country_capital string
 	Country_area    string
